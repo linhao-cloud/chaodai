219dc5729ab3e6caa3fcb57cbb9b445f572a4f6c
lin <1749195512@QQ.com>
2020-12-21 16:40:53 +0800

    推送

---
+++ src/main/java/com/hpe/testgit/test1/Apple.java
@@ -0,0 +1,10 @@
+package com.hpe.testgit.test1;
+
+public class Apple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("²âÊÔÍÆËÍ");
+	}
+
+}
